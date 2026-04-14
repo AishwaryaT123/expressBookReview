6e0d9b1ddd17383e515b659bbe137a420106e370
Aishwarya Tekade <157888799+AishwaryaT123@users.noreply.github.com>
2026-04-14 12:15:12 +0530

    Create general.js

---
+++ general.js
@@ -0,0 +1,11 @@
+# Async - Get all books
+curl -X GET http://localhost:5000/async/books
+
+# Async - Get by ISBN
+curl -X GET http://localhost:5000/async/isbn/1
+
+# Async - Get by author
+curl -X GET http://localhost:5000/async/author/Austen
+
+# Async - Get by title
+curl -X GET http://localhost:5000/async/title/Pride
